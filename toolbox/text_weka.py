59da7331753880c20cdd5e2a176c9617e13067a7
carlzhangxuan <carlzhangxuan@gmail.com>
2013-10-28 01:36:08 +0800

    weka converter

--- toolbox/text_weka.py
+++ toolbox/text_weka.py
@@ -12,5 +12,13 @@ __all__ = ['']
 
 #coding:utf8
 from itertools import *
+import text_sql_like as tl
 
-def python_2_weka(list_,label=0,query='')
+def python_2_weka(list_,label=0,query=1):
+	list_ = list(list_)
+	f1 = tl.text_keep_col(list_,[label,query])
+	if label != 1:
+		f1 = tl.text_change_col(f1,0,1)
+	f2 = tl.text_pop_col(list_, label)
+	return (f1,f2)
+	
