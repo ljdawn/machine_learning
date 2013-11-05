e3a7566e79d2e68d38c844b33083f18540874316
carlzhangxuan <carlzhangxuan@gmail.com>
2013-11-05 15:14:48 +0800

    not work sc

---
+++ clustering_Spectral/SC.py
@@ -0,0 +1,20 @@
+#coding:utf8
+"""
+====================
+SC not work
+====================
+
+"""
+__author__ = """\n""".join(['Xuan Zhang'])
+
+__version__ ="""2013-11-05"""
+
+__all__ = ['']
+
+import sys
+sys.path.append('..')
+import numpy as np
+from toolbox import text_manufactory
+from toolbox import text_chinese_filter
+from sklearn.cluster import spectral_clustering
+
