09f0a7407508020c62a93e2e63c24cf32cc82b03
zhang <carlzhangxuan@gmail.com>
2013-12-20 17:29:57 +0800

    1
    1

--- GLM/glm.py
+++ GLM/glm.py
@@ -1,7 +1,17 @@
 from sklearn import linear_model
+import numpy as np
+
 LLM = linear_model.LogisticRegression
 
 fn =  '../testing/data_glm'
 
 for line in open(fn):
-	print line
+	print line.strip()
+
+
+
+
+t = [1,2,3,4,5,6,7,8]
+tm  = np.array(t)
+
+print tm.reshape(2,4)
