c6acb405353f13b6fe16fe073abad9e856767610
zhang <carlzhangxuan@gmail.com>
2013-12-24 13:38:59 +0800

    C
    C

--- GLM/glm.py
+++ GLM/glm.py
@@ -1,7 +1,7 @@
 from sklearn import linear_model
 import numpy as np
 
-LLM = linear_model.LogisticRegression(tol = 1e-18, penalty = 'l1', C = 18)
+LLM = linear_model.LogisticRegression(tol = 1e-10, penalty = 'l1', C = 18)
 
 fn =  '../testing/data_glm'
 
@@ -22,11 +22,12 @@ print Res.predict(X)
 print Res.predict_proba(X)
 print y 
 
-print Res.get_params()
+print Res.coef_
+#print Res.get_params()
 
-
-
-
+#[[-0.12732452  5.38572702 -0.03768956  2.71767444  3.07732422]]
+#[[-0.0800647   1.21144739 -0.01576925  1.07963777  0.18775257]]
+#[[-0.12759553  5.39914591 -0.03771755  2.7191854   3.08301482]]
 
 #t = [1,2,3,4,5,6,7,8]
 #tm  = np.array(t)
