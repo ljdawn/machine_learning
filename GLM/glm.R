203fb5fde0cfa6ad4ae985d78e949d222ce35c44
zhang <carlzhangxuan@gmail.com>
2013-12-24 11:17:37 +0800

    C
    C

--- GLM/glm.R
+++ GLM/glm.R
@@ -1,4 +1,5 @@
 mydata <- read.table("../testing/data_glm")
 my_test <- glm(mydata$V6~mydata$V1+mydata$V2+mydata$V3+mydata$V4+mydata$V5, data=mydata,family = binomial("logit"))
 y_pre <- predict(my_test, data.frame(mydata$V1+mydata$V2+mydata$V3+mydata$V4+mydata$V5))
-print (exp(y_pre)/(1+exp(y_pre)))
+#print (exp(y_pre)/(1+exp(y_pre)))
+print (y_pre)
