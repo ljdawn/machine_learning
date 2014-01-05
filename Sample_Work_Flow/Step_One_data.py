757dc21c218482f52d23f64c14a341f29fbb00e3
zhang <carlzhangxuan@gmail.com>
2014-01-06 04:16:26 +0800

    flow_2

--- Sample_Work_Flow/Step_One_data.py
+++ Sample_Work_Flow/Step_One_data.py
@@ -1,8 +1,17 @@
-import pylab as pl
-from sklearn.datasets import load_digits
+import random
+import numpy as np
+from sklearn import svm, datasets
+from sklearn.metrics import precision_recall_curve
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import auc
 
-digits = load_digits()
-print digits.data.shape
+iris = datasets.load_iris()
 
-print digits.data[0]
-print digits.data[1000]
+data_train, data_test, label_train, label_test = train_test_split(iris.data[0:100], iris.target[0:100], train_size = 0.75, test_size = 0.25)
+
+classifier = svm.SVC(kernel='linear', probability=True)
+probas = classifier.fit(data_train, label_train)
+pred = np.array(probas.predict(data_test))
+
+print pred
+print type(label_test)
