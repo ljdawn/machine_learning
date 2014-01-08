4841e534a787b0107746cf3895428bc5b886b4cd
zhang <carlzhangxuan@gmail.com>
2014-01-08 11:07:17 +0800

    R_plot

--- Sample_Visualization/R/histogram_2.R
+++ Sample_Visualization/R/histogram_2.R
@@ -1,4 +1,7 @@
 library(ggplot2)
 CC = read.csv("cc_2_end_days", header = TRUE, sep = "\t")
+KB = read.csv("kb_2_end_days", header = TRUE, sep = "\t")
 p <- ggplot(data=CC,aes(x=times))
-p + geom_histogram(binwidth = 1) + labs(title = "cc 2 end") + xlab("days(binwidth = One day)") + ylab("times") + scale_x_continuous(limits=c(0, 1000))
+p1 <- ggplot(data=KB,aes(x=times))
+p + geom_histogram(binwidth = 7) + labs(title = "cc 2 end") + xlab("days(binwidth = One week)") + ylab("times") + scale_x_continuous(limits=c(1, 1000))
+p1 + geom_histogram(binwidth = 7) + labs(title = "kb 2 end") + xlab("days(binwidth = One week)") + ylab("times") + scale_x_continuous(limits=c(1, 1000))
