89bf680366db9744ba3fd5c9791ea482ab2d4fff
zhang <carlzhangxuan@gmail.com>
2014-01-19 21:59:50 +0800

    scipy

--- Numpy/Scipy_work.py
+++ Numpy/Scipy_work.py
@@ -1,2 +1,16 @@
 #scipy 
 #---Optimization and Minimization---
+"""data fitting sample"""
+import numpy as np
+from scipy.optimize import curve_fit
+
+def func(x, a, b):
+	return a * x + b
+
+x = np.linspace(1, 10, 100)
+y = func(x, 1, 2)
+yn = y + 0.9 * np.random.normal(size=len(x))
+popt, pcov = curve_fit(func, x, yn)
+print pcov
+#pcov, where the diagonal elements are the variances for each parameter.
+"""Solutions to Functions"""
