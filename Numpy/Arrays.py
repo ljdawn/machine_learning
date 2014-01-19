8e6a3c968519cc371027e5df57c49d63ff2075ca
zhang <carlzhangxuan@gmail.com>
2014-01-19 21:01:20 +0800

    numpy

---
+++ Numpy/Arrays.py
@@ -0,0 +1,19 @@
+import numpy as np
+#---array---
+#ndarray is ~25 faster then python loops.
+#matrix can only will be two dimensional.
+
+#array creation samples
+"""single value"""
+arr = np.array(100)
+"""range values"""
+arr = np.arange(0,100)
+"""step values"""
+arr = np.linspace(0,1,100)
+arr = np.logspace(0, 1, 100, base=10.0)
+"""0s, 1s"""
+arr = np.zeros(5)
+arr = np.ones(5)
+cube = np.zeros((5,5,5)).astype(int) + 1
+
+#---array can store other type data---
