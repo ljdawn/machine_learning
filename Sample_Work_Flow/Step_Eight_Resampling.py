5ec9a19f46a94b751a6073291df35178136d10a6
zhang <carlzhangxuan@gmail.com>
2014-01-20 22:16:15 +0800

    resampling

--- Sample_Work_Flow/Step_Eight_Resampling.py
+++ Sample_Work_Flow/Step_Eight_Resampling.py
@@ -1,1 +1,8 @@
 #http://scikit-learn.org/dev/modules/generated/sklearn.utils.resample.html
+import numpy as np
+
+X = [[1., 0.], [2., 1.], [0., 0.],[8., 4.], [5., -5], [7., 8.]]
+y = np.array([0, 1, 2])
+
+from sklearn.utils import resample
+print resample(X, replace = False, n_samples = 5, random_state = 25)
