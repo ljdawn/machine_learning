8cae44c2c6fafa07da14f591a1c10224f3ebc489
zhang <carlzhangxuan@gmail.com>
2014-02-19 15:22:16 +0800

    stats models

---
+++ StatsModels/Comparison_between_two_lists/Step_One.py
@@ -0,0 +1,23 @@
+"""
+====================
+data comparision
+Two
+====================
+
+"""
+__author__ = """\n""".join(['Xuan Zhang'])
+
+__version__ ="""2013-02-19"""
+
+__all__ = ['']
+import pandas as pd
+import numpy as np
+import scipy as sp
+import statsmodels.api as sm
+
+l_path = 'data/wt_sample3.out'
+r_path = 'data/pred_sample_3'
+head_path = 'data/head.txt'
+#histogram
+
+
