e12eed833ae1230d10a86aa18f19b750a34616e0
zhang <carlzhangxuan@gmail.com>
2014-02-19 16:33:36 +0800

    t

--- StatsModels/Comparison_between_two_lists/Step_One.py
+++ StatsModels/Comparison_between_two_lists/Step_One.py
@@ -15,9 +15,37 @@ import numpy as np
 import scipy as sp
 import statsmodels.api as sm
 
-l_path = 'data/wt_sample3.out'
-r_path = 'data/pred_sample_3'
-head_path = 'data/head.txt'
-#histogram
+def range_histogram(list_left, list_right):
+	list_left = np.array(list_left)
+	list_right = np.array(list_right)
+	range_upper = max(list_left.max(), list_right.max())
+	range_lower = min(list_left.min(), list_right.min())
+	range_all = range_upper - range_lower
+	return range(int(range_all + 2))
 
+def view_histogram(list_left, list_right, range_all):
+	(left_val, bar) = np.histogram(list_left, bins = range_all)
+	(right_val, bar) = np.histogram(list_right, bins = range_all)
+	return (left_val, right_val, bar)
 
+def single_ols(X, y):
+	return sm.OLS(y, X).fit().summary()
+
+if __name__ == '__main__':
+    l_path = 'data/wt_sample3.out'
+    r_path = 'data/pred_sample_3'
+    head_path = 'data/head.txt'
+    clo_name = 'site_type'
+    clo_an = 'chengdan_flag'    
+
+    clo_n = [cle.strip() for cle in open(head_path, 'r').readlines()]
+    f_l = pd.read_table(l_path, names = clo_n, sep = '\t')
+    f_r = pd.read_table(r_path, names = clo_n, sep = '\t')
+
+    f_l_c = f_l[clo_name]
+    f_l_a = f_l[clo_an]
+    f_r_c = f_r[clo_name]
+    f_r_a = f_r[clo_an]
+    print clo_name, ':', view_histogram(f_l_c, f_r_c, range_histogram(f_l_c, f_r_c))
+    print single_ols(f_l_c, f_l_a)
+    #print single_ols(f_r_c, f_r_a)
