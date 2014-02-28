e92cd32ff36cbf76992d4fb439712205d6549d8f
zhang <carlzhangxuan@gmail.com>
2014-02-28 18:32:09 +0800

    new functions

--- Sample_Work_Space/tool_box/Step_Zero_Processing_v08.py
+++ Sample_Work_Space/tool_box/Step_Zero_Processing_v08.py
@@ -12,12 +12,39 @@ __version__ = """2013-02-28"""
 __all__ = ['']
 
 #coding:utf8
-def split_data_matrix(data_matrix, colnames = None, binar_list = None, discret_list = None):
+def split_data_matrix(data_matrix, colnames = None, value_list = None, binar_list = None, discret_list = None):
     import pandas as pd
+    if colnames is None: colnames = range(len(data_matrix[0]))
+    df_data = pd.DataFrame(data_matrix, columns = colnames)
+    if value_list is not None: yield df_data[value_list]
+    if binar_list is not None: yield df_data[binar_list]
+    if discret_list is not None: yield df_data[discret_list]
 
+def scale_value_list(data_matrix, mode = 0):
+    import numpy as np
+    from sklearn import preprocessing
+    target = [map(float, x) for x in np.array(data_matrix)]
+    min_max_scaler = preprocessing.MinMaxScaler()
+    StandardScaler = preprocessing.StandardScaler()
+    model = min_max_scaler.fit(target) if mode is 0 else StandardScaler.fit(target)
+    return model.transform(target)
 
+def my_binarizer(data_matrix, threshold):
+    from sklearn import preprocessing
+    import numpy as np
+    binarizer = preprocessing.Binarizer(threshold)
+    return binarizer.transform(np.array(data_matrix))
+
+def scale_binar_list(data_matrix, binar_thr_list = None):
+    import numpy as np
+    if binar_thr_list is None:
+        binar_thr_list = line:np.array(line).mean() for line in data_matrix
 
 if __name__ == '__main__':
-    import pandas as pd
-    a = pd.DataFrame({'a':[1, 2, 3], 'b':[1, 2, 3], 'c':[1, 2, 3], 'd':[1, 2, 3]})
+    import numpy as np
+    data = [[1, 2, 3, 13], [4, 5, 6, 7], [7, 8, 9, 13], [10, 11, 12, 14]]
+    data_title = ['a', 'b', 'c', 'd']
+    #a = pd.DataFrame({'a':[1, 2, 3], 'b':[1, 2, 3], 'c':[1, 2, 3], 'd':[1, 2, 3]})
+    test = split_data_matrix(data, data_title,['a', 'c'], ['c', 'd'])
+    a = scale_value_list(test.next(), 1)
     print a
