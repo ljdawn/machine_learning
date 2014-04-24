506e96ff868c760e5d9539689c2f61c8ba7c337e
carlzhangxuan <carlzhangxuan@gmail.com>
2014-04-24 23:46:49 +0800

    la

--- PyMC_MCMC/poi_distri.py
+++ PyMC_MCMC/poi_distri.py
@@ -10,9 +10,9 @@ poi = stats.poisson
 lambda_ = [1.5, 4.25]
 colours = ["#348ABD", "#A60628"]
 
-plt.bar(a, poi.pmf(a, lambda_[0]), color=colours[0], label="$\lambda = %.1f$" % lambda_[0], alpha=0.60, edgecolor=colours[0], lw="3")
+plt.bar(a, poi.pmf(a, lambda_[0]), color=colours[0], label="$\lambda = %.1f$" % lambda_[0], alpha=0.60, edgecolor=colours[0], lw=3.0)
 
-plt.bar(a, poi.pmf(a, lambda_[1]), color=colours[1], label="$\lambda = %.1f$" % lambda_[1], alpha=0.60, edgecolor=colours[1], lw="3")
+plt.bar(a, poi.pmf(a, lambda_[1]), color=colours[1], label="$\lambda = %.1f$" % lambda_[1], alpha=0.60, edgecolor=colours[1], lw=3.0)
 
 plt.xticks(a + 0.4, a)
 plt.legend()
