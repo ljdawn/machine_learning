89ca90c095ee0d44aa135c4b28929fc26c037eb2
carlzhangxuan <carlzhangxuan@gmail.com>
2014-04-26 17:23:36 +0800

    new

--- PyMC_MCMC/mc_1.py
+++
@@ -1,6 +0,0 @@
-import pymc as pm
-with pm.Model() as model:
-	parameter = pm.Exponential("poisson_param",  1)
-	data_generator = pm.Poisson("data_generator", parameter)
-	data_plus_one = data_generator + 1
-print data_plus_one
