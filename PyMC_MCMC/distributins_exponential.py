878644e2c523fd3a8f85a264af11ce6734d5d68f
carlzhangxuan <carlzhangxuan@gmail.com>
2014-04-28 01:52:14 +0800

    exp

--- PyMC_MCMC/distributins_exponential.py
+++ PyMC_MCMC/distributins_exponential.py
@@ -1,7 +1,7 @@
 from IPython.core.pylabtools import figsize
 import numpy as np
 from matplotlib import pyplot as plt
-figsize(11, 9)
+figsize(11, 5)
 
 import scipy.stats as stats
 from scipy.stats import expon
@@ -11,28 +11,31 @@ import matplotlib
 s = json.load(open("style/bmh_matplotlibrc.json"))
 matplotlib.rcParams.update(s)
 
-trials = [1,2,5,9]
-x = np.arange(-1, 18, 1)
+trials = [0.5,1.0,1.5,2.0]
+x = np.linspace(0, 5, 25)
 
 for k, l in enumerate(trials):
-	sx = plt.subplot(len(trials), 2, k*2 + 1)
-	rv = poisson(l)
-	y = rv.pmf(x)
-	plt.bar(x-0.2, y, width=0.4,label="Lambda: %d" % l, color = "#348ABD")
-	#plt.vlines(l, 0, 0.5, color="k", linestyles="--", lw=1)
+	#sx = plt.subplot(len(trials), 2, k*2 + 1)
+	sx = plt.subplot(1, 2, 1)
+	rv = expon(scale = 1/l)
+	y = rv.pdf(x)
+	plt.plot(x, y, label="Lambda: %0.1f" % l)
+	#plt.fill_between(x, 0, y, color="#348ABD", alpha=0.1)
 	leg = plt.legend()
 	leg.get_frame().set_alpha(0.4)
 	plt.autoscale(tight=True)
-	sx = plt.subplot(len(trials), 2, k*2 + 2)
-	rv = poisson(l)
+	#sx = plt.subplot(len(trials), 2, k*2 + 2)
+	sx = plt.subplot(1, 2, 2)
+	rv = expon(scale = 1/l)
 	y = rv.cdf(x)
-	plt.bar(x-0.2, y, width=0.4,label="Lambda: %d" % l, color = "#348ABD")
-	#plt.vlines(l, 0, 0.5, color="k", linestyles="--", lw=1)
+	plt.plot(x, y, label="Lambda: %0.1f" % l)
+	plt.fill_between(x, 0, y, color="#348ABD", alpha=0.1)
 	leg = plt.legend()
 	leg.get_frame().set_alpha(0.4)
 	plt.autoscale(tight=True)
 
 
-plt.suptitle("exponential distribution", y=0.99, fontsize=14)
+
+plt.suptitle("exponential distribution", y=1.0, fontsize=14)
 plt.tight_layout()
 plt.show()
