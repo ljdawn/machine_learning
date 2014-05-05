b30a723cdeb08497c82fb718eaf10e4dedf2201e
zhang <carlzhangxuan@gmail.com>
2014-05-05 17:21:40 +0800

    mcmc

--- PyMC_MCMC/mc_matrix.py
+++ PyMC_MCMC/mc_matrix.py
@@ -5,6 +5,6 @@ m_tran = np.array([[0.65, 0.28, 0.07],[0.15, 0.67, 0.18],[0.12, 0.36, 0.52]])
 #m = np.array([0.21, 0.68, 0.11])
 m = np.array([0.1, 0.8, 0.1])
 
-for i in range(10):
+for i in range(100):
 	m =  m.dot(m_tran)
 	print m
