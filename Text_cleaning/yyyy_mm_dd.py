e39028f990b849a9d2105457388fccde1adbaa40
zhang <carlzhangxuan@gmail.com>
2014-05-13 23:26:11 +0800

    datetime done

--- Text_cleaning/yyyy_mm_dd.py
+++ Text_cleaning/yyyy_mm_dd.py
@@ -1,13 +1,34 @@
+#coding:utf8
 import re
 
-rule_1 = '(1[6-9]\d{2}|2[0-1]\d{2})([-\/\._ ])(10|12|0?[13578])([-\/\._ ])(3[01]|[12][0-9]|0?[1-9])$'
-rule_2 = '(1[6-9]\d{2}|2[0-1]\d{2})([-\/\._ ])(11|0?[469])([-\/\._ ])(30|[12][0-9]|0?[1-9])$'
-rule_3 = '(1[6-9]\d{2}|2[0-1]\d{2})([-\/\._ ])(0?2)([-\/\._ ])(2[0-8]|1[0-9]|0?[1-9])$'
-rule_4 = '(2000)([-\/\._ ])(0?2)([-\/\._ ])(29)$'
-rule_5 = '['' ]'
+def find_datatime_in_string(text):
+	rule_1 = '(1[8-9]\d{2}|2[0-1]\d{2})([-\/\._ ])(10|12|0?[13578])([-\/\._ ])(3[01]|[12][0-9]|0?[1-9])$'
+	rule_2 = '(1[8-9]\d{2}|2[0-1]\d{2})([-\/\._ ])(11|0?[469])([-\/\._ ])(30|[12][0-9]|0?[1-9])$'
+	rule_3 = '(1[8-9]\d{2}|2[0-1]\d{2})([-\/\._ ])(0?2)([-\/\._ ])(2[0-8]|1[0-9]|0?[1-9])$'
+	rule_4 = '(2000)([-\/\._ ])(0?2)([-\/\._ ])(29)' 
+	lyear = range(1800, 2100)
+	leap_year = []
+	for year in lyear:
+		if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+			leap_year.append(str(year))
+	rule_5_h = '|'.join(leap_year)
+	rule_5 = 'rule_5_h'+'([-\/\._ ])(0?2)([-\/\._ ])(29)$'
+	rule = re.compile(rule_1+'|'+rule_2+'|'+rule_3+'|'+rule_4)
+	rule_0 = re.compile('\d{4}[-\/\._ ]\d{2}[-\/\._ ]\d{2}')
+	ans = []
+	for st in rule_0.findall(text):
+		try:
+			ans.append(rule.match(st).group())
+		except:
+			pass
+	return ans if ans != [] else ['nothing']
 
-rule = re.compile((rule_1+'|'+rule_2+'|'+rule_3+'|'+rule_4))
+def main():
+	text = 'zhognguo工商局 2651 ewf 1985-89-98 fafe//,dlmcv a2014 02_29 ae f a 2000-02-291922 02-02'
+	print 'input text :', text
+	print 'we have found :',
 
-test = 'adfv 2000-2-29 '
+	print [datatime for datatime in find_datatime_in_string(text)]
 
-print rule.findall(test) if rule.findall(test) != [] else 'nothing here!'
+if __name__ == '__main__':
+	main()
