de0e93069eb0673f72f4273d7313b3d0fa3db1e3
zhang <carlzhangxuan@gmail.com>
2014-06-11 18:17:30 +0800

    graph

--- Igraph/community_detection/cd.py
+++ Igraph/community_detection/cd.py
@@ -1,7 +1,9 @@
 import igraph as ig
 import networkx as nx
 
-G = nx.gnm_random_graph(100, 700, seed=1)
+#G = nx.gnm_random_graph(100, 700, seed=1)
+G = nx.gnm_random_graph(100, 700)
+
 
 GI = ig.Graph()
 GI.add_vertices(G.nodes())
@@ -9,15 +11,23 @@ GI.add_edges(G.edges())
 
 p = ig.Graph.pagerank(GI)
 
-c = ig.Graph.community_label_propagation(GI)
+c5 = ig.Graph.community_label_propagation(GI)
 c1 = ig.Graph.community_multilevel(GI)
+c2 = ig.Graph.community_spinglass(GI)
+c4 = ig.Graph.community_infomap(GI)
+c3 = ig.Graph.community_leading_eigenvector(GI)
 
 g = 0
-res_dic = {}
-for cls in c1:
-	for ele in cls:
-		res_dic[ele] = g
-	g += 1
+#res_dic = {}
+#for cls in c2:
+#	for ele in cls:
+#		res_dic[ele] = g
+#	g += 1
 
-print res_dic
+#print res_dic
 
+print c1
+print c2
+print c3
+print c4
+print c5
