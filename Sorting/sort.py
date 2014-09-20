6866464dc7e698fb3f372628d2bfd259e65662b0
carlzhangxuan <carlzhangxuan@gmail.com>
2014-09-20 19:19:45 +0800

    test
    test

--- Sorting/sort.py
+++ Sorting/sort.py
@@ -19,27 +19,26 @@ def insertion_sort(List):
 			List = right_l[:i] + [List[0]] + right_l[i:]
 	return [List[0]] + right_l
 
-def quick_sub_sort(array, low, high):
-	key = array[low]
-	while low < high:
-		while low < high and key <= array[high]:
-			high -= 1
-		while low < high and key > array[high]:
-			array[low] = array[high]  
-			low += 1
-			array[high] = array[low]  
-	array[low] = key  
-	return low
+def quick_sort(arr):
+	if len(arr) < 1:
+		return arr
+	key = arr[0]
+	left = []
+	right = []
+	for i in xrange(1, len(arr)):
+		if arr[i] <= key:
+			left.append(arr[i])
+		else:
+			right.append(arr[i])
+	left = quick_sort(left)
+	right = quick_sort(right)
+	return left + [key] + right
 
-def quick_sort(array, low, high):
-	if low < high :  
-		key_index = quick_sub_sort(array,low,high)
-		quick_sort(array, low, key_index)  
-		quick_sort(array, key_index+1, high)
+		
 
 if __name__ == '__main__':
 	test_list = [12,3,51,3,2,5,78,88,34,25,2,1000]
-	print bubble_sort(test_list)
-	print selection_sort(test_list)
-	print insertion_sort(test_list)
-	print quick_sort(test_list, 0, len(test_list)-1)
+	#print bubble_sort(test_list)
+	#print selection_sort(test_list)
+	#print insertion_sort(test_list)
+	#print quick_sort(test_list)
