2e980fa62c995bc7e2b5ecd801d48742aaf63e14
carlzhangxuan <carlzhangxuan@gmail.com>
2014-10-02 01:33:54 +0800

    dpl

--- Deep_Learning/theano_basic/basic.py
+++ Deep_Learning/theano_basic/basic.py
@@ -2,6 +2,7 @@ import numpy as np
 import theano
 import theano.tensor as T
 from theano import shared
+from theano.tensor.shared_randomstreams import RandomStreams
 
 def basic():
 	x = T.dvector('x')
@@ -20,8 +21,28 @@ def derivation():
 	print f(a)
 
 def shared_v():
-	
+	n = shared(np.array([1.5, 2.5, 3.5]), name='n')
+	print n.get_value()
+	f = theano.function(inputs=[], outputs=[], updates={n: n * 2})
+	f()
+	print n.get_value()
+	f()
+	print n.get_value()
+
+def random_num():
+	rng = RandomStreams(1234)
+	x = rng.normal((10,))
+	y = rng.binomial((10,), p=0.5, n=1)
+	z = x * y
+	f = theano.function(inputs=[], outputs=[x, y, z])
+	f()
+	print x, y, z
+	f()
+	print x, y, z
+
 
 if __name__ == '__main__':
 	#basic()
-	derivation()
+	#derivation()
+	#shared_v()
+	random_num()
