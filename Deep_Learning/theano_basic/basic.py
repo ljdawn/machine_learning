5a7768b49bb04f9c1a513269ec804c2ccc952e5c
carlzhangxuan <carlzhangxuan@gmail.com>
2014-10-02 00:08:04 +0800

    dp

--- Deep_Learning/theano_basic/basic.py
+++ Deep_Learning/theano_basic/basic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import theano
 import theano.tensor as T
+from theano import shared
 
 def basic():
 	x = T.dvector('x')
@@ -9,5 +10,18 @@ def basic():
 	a = np.array([1.,2.,5.])
 	print f(a)
 
+def derivation():
+	x = T.dvector('x')
+	y = x**2
+	z = T.sum(x**2 + y)
+	gz = T.grad(cost=z, wrt=x)
+	f = theano.function(inputs=[x], outputs=gz)
+	a = np.array([1.,2.,3.])
+	print f(a)
+
+def shared_v():
+	
+
 if __name__ == '__main__':
-	basic()
+	#basic()
+	derivation()
