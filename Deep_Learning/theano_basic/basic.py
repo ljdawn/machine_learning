924b5c18c440361c2fe715d5cc54b282f970a641
carlzhangxuan <carlzhangxuan@gmail.com>
2014-10-01 23:47:33 +0800

    dp

--- Deep_Learning/theano_basic/basic.py
+++ Deep_Learning/theano_basic/basic.py
@@ -1,2 +1,13 @@
+import numpy as np
 import theano
 import theano.tensor as T
+
+def basic():
+	x = T.dvector('x')
+	y = x**2
+	f = theano.function(inputs=[x], outputs=y)
+	a = np.array([1.,2.,5.])
+	print f(a)
+
+if __name__ == '__main__':
+	basic()
