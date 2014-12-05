b0ddcd72e7680aa02b01c30f94326f696992cd9c
carlzhangxuan <carlzhangxuan@gmail.com>
2014-12-05 17:06:19 +0800

    deep
    deep

---
+++ Deep_Learning/theano_basic/logistic_regression.py
@@ -0,0 +1,13 @@
+import cPickle
+import gzip
+import os
+import sys
+import time
+import numpy
+import theano
+import theano.tensor as T
+
+class logistic_regression(object):
+
+	def __init__(self, input, n_in, n_out):
+		pass
